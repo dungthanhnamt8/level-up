28602dadfe0f75ae0d9f31d5ed63a398a4f33928
Phan Xuân Dũng <114124789+vaix-dungpx@users.noreply.github.com>
2024-01-17 06:47:00 +0700

    Update

--- src/Models/Level.php
+++ src/Models/Level.php
@@ -5,6 +5,7 @@ namespace LevelUp\Experience\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use LevelUp\Experience\Exceptions\LevelExistsException;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 use Throwable;
 
 class Level extends Model
@@ -47,5 +48,128 @@ class Level extends Model
     public function users(): HasMany
     {
         return $this->hasMany(related: config(key: 'level-up.user.model'));
+    }
+
+    use QueryCacheable;
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Do not specify or set it to null to disable caching.
+     *
+     * @var int|\DateTime
+     */
+    protected $cacheFor = 604800; // 1 week
+
+    /**
+     * The tags for the query cache. Can be useful
+     * if flushing cache for specific tags only.
+     *
+     * @var null|array
+     */
+    public $cacheTags = ['levels'];
+
+    /**
+     * A cache prefix string that will be prefixed
+     * on each cache key generation.
+     *
+     * @var string
+     */
+    public $cachePrefix = 'levels_';
+
+    /**
+     * The cache driver to be used.
+     *
+     * @var string
+     */
+    //public $cacheDriver = 'dynamodb';
+
+    /**
+     * Set the base cache tags that will be present
+     * on all queries.
+     */
+    protected function getCacheBaseTags(): array
+    {
+        return [
+            'custom_level_tag',
+        ];
+    }
+
+    /**
+     * When invalidating automatically on update, you can specify
+     * which tags to invalidate.
+     *
+     * @param  string|null  $relation
+     * @param  \Illuminate\Database\Eloquent\Collection|null  $pivotedModels
+     */
+    public function getCacheTagsToInvalidateOnUpdate($relation = null, $pivotedModels = null): array
+    {
+        return [
+            "level:{$this->id}",
+            'levels',
+        ];
+    }
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Set it to null to disable caching.
+     *
+     * @return int|\DateTime
+     */
+    protected function cacheForValue()
+    {
+        //is local
+        if (app()->environment('local')) {
+            return null;
+        }
+
+        return $this->cacheFor;
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::created(function (Level $agent) {
+            // ...
+            $agent::flushQueryCache(['test']);
+        });
+        //Update
+        static::saved(function (Level $agent) {
+            // ...
+            $agent::flushQueryCache(['test']);
+        });
+
+        //Delete
+        static::deleted(function (Level $agent) {
+            // ...
+            $agent::flushQueryCache(['test']);
+        });
+    }
+
+    //flushQueryCacheItem
+    public function flushQueryCacheItem()
+    {
+        $cacheKeyAgent = 'level_cache_for';
+        //Delete cache
+        cache()->forget($cacheKeyAgent);
+
+        return true;
+    }
+
+    protected function getCacheForKey()
+    {
+        return 'level_cache_for';
+    }
+
+    /**
+     * The tags for the query cache. Can be useful
+     * if flushing cache for specific tags only.
+     *
+     * @return null|array
+     */
+    protected function cacheTagsValue()
+    {
+        return ['levels'];
     }
 }
