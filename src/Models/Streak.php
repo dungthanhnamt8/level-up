5031164bde5dad346c9b97c1cdd7c5b5b79117fe
Phan Xuân Dũng <dungpx@vaixgroup.com>
2024-02-15 11:48:00 +0700

    Update

--- src/Models/Streak.php
+++ src/Models/Streak.php
@@ -5,6 +5,7 @@ namespace LevelUp\Experience\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Streak extends Model
 {
@@ -25,5 +26,128 @@ class Streak extends Model
     public function activity(): BelongsTo
     {
         return $this->belongsTo(related: Activity::class);
+    }
+
+    use QueryCacheable;
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Do not specify or set it to null to disable caching.
+     *
+     * @var int|\DateTime
+     */
+    protected $cacheFor = 604800; // 1 week
+
+    /**
+     * The tags for the query cache. Can be useful
+     * if flushing cache for specific tags only.
+     *
+     * @var null|array
+     */
+    public $cacheTags = ['streaks'];
+
+    /**
+     * A cache prefix string that will be prefixed
+     * on each cache key generation.
+     *
+     * @var string
+     */
+    public $cachePrefix = 'streaks_';
+
+    /**
+     * The cache driver to be used.
+     *
+     * @var string
+     */
+    //public $cacheDriver = 'dynamodb';
+
+    /**
+     * Set the base cache tags that will be present
+     * on all queries.
+     */
+    protected function getCacheBaseTags(): array
+    {
+        return [
+            'custom_streak_tag',
+        ];
+    }
+
+    /**
+     * When invalidating automatically on update, you can specify
+     * which tags to invalidate.
+     *
+     * @param  string|null  $relation
+     * @param  \Illuminate\Database\Eloquent\Collection|null  $pivotedModels
+     */
+    public function getCacheTagsToInvalidateOnUpdate($relation = null, $pivotedModels = null): array
+    {
+        return [
+            "streak:{$this->id}",
+            'streaks',
+        ];
+    }
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Set it to null to disable caching.
+     *
+     * @return int|\DateTime
+     */
+    protected function cacheForValue()
+    {
+        //is local
+        if (app()->environment('local')) {
+            return null;
+        }
+
+        return $this->cacheFor;
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::created(function (Streak $agent) {
+            // ...
+            $agent::flushQueryCache(['streaks']);
+        });
+        //Update
+        static::saved(function (Streak $agent) {
+            // ...
+            $agent::flushQueryCache(['streaks']);
+        });
+
+        //Delete
+        static::deleted(function (Streak $agent) {
+            // ...
+            $agent::flushQueryCache(['streaks']);
+        });
+    }
+
+    //flushQueryCacheItem
+    public function flushQueryCacheItem()
+    {
+        $cacheKeyAgent = 'streak_cache_for';
+        //Delete cache
+        cache()->forget($cacheKeyAgent);
+
+        return true;
+    }
+
+    protected function getCacheForKey()
+    {
+        return 'streak_cache_for';
+    }
+
+    /**
+     * The tags for the query cache. Can be useful
+     * if flushing cache for specific tags only.
+     *
+     * @return null|array
+     */
+    protected function cacheTagsValue()
+    {
+        return ['streaks'];
     }
 }
